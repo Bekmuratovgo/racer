801b38983298ff1ee22d34c0dd0ba82919420d3d
Nurbek <nurbekbekmuratov03@gmail.com>
2023-08-17 18:09:59 +0600

    deploy

--- src/store/table.action.js
+++ src/store/table.action.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { tableSlice } from "./table.slice";
 
+const url = process.env.REACT_APP_BASE_URL
 export const getUsers = (page = 1) => async dispatch => {
   dispatch(tableSlice.actions.setLoading(true))
   try {
-    const res = await axios(`https://devapi.almurut.com/api/test/racers/?page=${page}`)
+    const res = await axios(`${url}/?page=${page}`);
     dispatch(tableSlice.actions.setUsersuccess(res.data.results.slice(0, 50)))
   } catch (error) {
     console.log(error, 'ERR');
